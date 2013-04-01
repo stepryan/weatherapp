40ba04200d35eb63d45644a27ca7c520894c399a
stephen ryan <stephen.ryan2@student.ncirl.ie>
2013-04-01 22:08:18 +0100

    fixed problem in weatherapp gem rebuilt gems

--- weatherapp.rb
+++ weatherapp.rb
@@ -2,7 +2,7 @@ module CheckWeather
 require "xmlrpc/client" 
 @parameters = weather.seventimer.returnastro
 @url = "http://www.7timer.com/v4/bin/astro.php?"
-server = XMLRPC::Client.new( @url + @ parameters)
+server = XMLRPC::Client.new( @url + @parameters)
 
 def AstroResult
     @astroresult = server.call()
