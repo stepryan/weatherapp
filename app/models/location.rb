fa301d83e94696c0956e850885c458ef46c2683d
stephen ryan <stephen.ryan2@student.ncirl.ie>
2013-04-01 17:00:30 +0100

    updated location model again to fix post problem

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,7 @@
 class Location < ActiveRecord::Base
   attr_accessible :address, :latitude, :longitude, :name,:gmaps
   belongs_to :user
+  accepts_nested_attributes_for :user
   acts_as_gmappable 
       def gmaps4rails_address
          address
